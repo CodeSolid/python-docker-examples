602f43ebd2d870bc30f13660bec09bf191b301ea
John Lockwood <elitepropertiesbroker@gmail.com>
2022-04-03 10:41:40 -0400

    Stable 03 Django finally

--- 03-docker-compose-django/codesolid_demo/codesolid_demo/asgi.py
+++ 03-docker-compose-django/codesolid_demo/codesolid_demo/asgi.py
@@ -1,11 +1,11 @@
-
+"""
 ASGI config for codesolid_demo project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
-
+"""
 
 import os
 
--- 03-docker-compose-django/codesolid_demo/codesolid_demo/wsgi.py
+++ 03-docker-compose-django/codesolid_demo/codesolid_demo/wsgi.py
@@ -1,11 +1,11 @@
-
+"""
 WSGI config for codesolid_demo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-
+"""
 
 import os
 
